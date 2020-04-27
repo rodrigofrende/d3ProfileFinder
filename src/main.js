fcab2c503ee106a0bc1ad79fa18b7e54e3b827c6
rfrende <rfrende@consensusgroup.net>
2020-04-27 09:07:41 -0300

    orden en el main , librerias separadas

--- src/main.js
+++ src/main.js
@@ -4,37 +4,10 @@ import router from './router'
 import store from './store'
 
 // importar librerias
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import './assets/css/main.styl' 
+import './plugins/bootstrapVue'  //importo los componentes en archivo separados , para hacer el codigo mas limpio
+import './plugins/fontAwesome'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-// Iconos de tipo "Solid"
-import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
-// Iconos de tipo "Brand" (marcas o logos de empresas)
-import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
-// El componente que vamos a utilizar
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import './assets/css/main.styl'  // css global
-
-library.add(
-  faSkull,
-  faCrown,
-  faDungeon,
-  faHatWizard,
-  faHammer,
-  faGem,
-  faVuejs,
-  faBootstrap,
-  faFontAwesome,
-  faGithub,
-  faBattleNet
-)
-
-// Dar de alta componentes
-Vue.use(BootstrapVue)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
