816c5954872b17a58706505985cdd5410f65f344
rfrende <rfrende@consensusgroup.net>
2020-04-28 10:49:14 -0300

    Cambio en vueRouter , indices con lazyLoad . (no se si es necesario este cambio, quizas se revierta)

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,37 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/home/Index.vue'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/about/Index.vue')
-  }
+const routerOptions = [
+  { path: '/', name: 'Home' },
+  { path: '/region/:region/profile/:battleTag', name: 'Profile' },
+  { path: '/region/:region/profile/:battleTag/hero/:heroId', name: 'Hero' },
+  { path: '/about', name: 'About' },
+  { path: '/error', name: 'Error' },
+  { path: '*', redirect: { name: 'Home' } }
 ]
+//los dos puntos (:) en el path hacen referencia a un parámetro o variable en la ruta.
+const routes = routerOptions.map(r => {
+  return {
+    ...r,
+    component: () => import(/* webpackChunkName: "[request]" */`@/views/${r.name}/Index.vue`)
+  }
+})
 
-const router = new VueRouter({
+const router = new Router({
   routes
 })
+
+// const router = new VueRouter({
+//   routes
+// })
+
+//npm run build , genera bloques de codigo llamados chunk , estos se puede renombrar 
+// {
+//     path: '/',
+//     name: 'Home',
+//     component: () => import(/* webpackChunkName: "Home" */ '@/views/Home/Index.vue')
+// }
 
 export default router
--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="Home">
     <HomeTitle/> 
     <HomeForm/>
   </div>
